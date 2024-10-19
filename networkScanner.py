789dd19bcc0a88c5c122538a9a2a1c604b65eb60
codemaxx1 <garrnic3@isu.edu>
2024-10-18 21:56:32 -0600

    network scanner

--- networkScanner.py
+++ networkScanner.py
@@ -36,35 +36,48 @@ class Scanner:
 
     # scan the network for devices
     def scan(self, type, IP, netMask):
+        port = 80
 
-        # tcp scan
-        if type == "TCP":
-            return 1
+        # #of hosts = 2^bits - 2 -- I have -1 because the range starts at 1, so the first -1 wouldn't be needed
+        hosts_range = (2 ** (32 - (int(netMask)))) - 1
+
+        logger.log(f'netMaskRange = {hosts_range}', 'networkScanner')
+        activeIPs = []
+        for i in range(1, hosts_range):
+            ip = IP.split('.')
+            ip = f"{ip[0]}.{ip[1]}.{ip[2]}.{i}"
+
+            # tcp scan
+            if type == "TCP":
+                logger.log(f'TCP pinging ip = {ip}', 'networkScanner')
+
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                # set a timeout
+                socket.setdefaulttimeout(1)
+
+                try:
+                    # returns 0 if connection succeeds else raises error
+                    if s.connect_ex((IP, port)):
+                        activeIPs.append(IP)
+                except:
+                    logger.log(f'no connection on {IP}:{port}')
+
+                s.close()
 
 
-        # ping type scan
-        elif type == "ICMP":
-            # #of hosts = 2^bits - 2 -- I have -1 because the range starts at 1, so the first -1 wouldn't be needed
-            hosts_range = (2**(32-(int(netMask)))) - 1
+            # ping type scan
+            elif type == "ICMP":
+                    logger.log(f'ICMP pinging ip = {ip}', 'networkScanner')
 
-            logger.log(f'netMaskRange = {hosts_range}', 'networkScanner')
-            activeIPs = []
-            for i in range(1,hosts_range):
-
-                ip = IP.split('.')
-                ip = f"{ip[0]}.{ip[1]}.{ip[2]}.{i}"
-
-                logger.log(f'ICMP pinging ip = {ip}', 'networkScanner')
-
-                #                                    -c --> 1 ping, -W --> timeout
-                pingResponse = subprocess.call(['ping', '-c', '1', '-W', '1', ip])
-                if pingResponse == 0:
-                    logger.say(f'active device on {ip}')
-                    activeIPs.append(ip)
-                elif pingResponse == 2:
-                    logger.log(f"no response from {ip}", 'networkScanner')
-                else:
-                    logger.log(f"ping to {ip} failed", 'networkScanner')
+                    #                                    -c --> 1 ping, -W --> timeout
+                    pingResponse = subprocess.call(['ping', '-c', '1', '-W', '1', ip])
+                    if pingResponse == 0:
+                        logger.say(f'active device on {ip}')
+                        activeIPs.append(ip)
+                    elif pingResponse == 2:
+                        logger.log(f"no response from {ip}", 'networkScanner')
+                    else:
+                        logger.log(f"ping to {ip} failed", 'networkScanner')
 
             logger.log(f'\nactive ips: {activeIPs}', 'networkScanner')
 
